bafdbce515e1bdf21ee659b4d36bd30c22d3b8a5
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-08-18 12:10:05 +0300

    Add prefetch integration to Astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), prefetch()],
   server: {
     host: true,
   },
