be0d6748195fcb3d1cf08d42ad598420b0ada4ed
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-08-19 00:07:24 +0300

    Add svgr plugin to Vite

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,12 +3,16 @@ import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
+import svgr from 'vite-plugin-svgr';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [react(), tailwind(), prefetch()],
+  vite: {
+    plugins: [svgr()],
+  },
   server: {
     host: true,
   },
