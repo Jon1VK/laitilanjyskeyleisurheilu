311818c1316134e8ead63ddfe5a60f974f10a886
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-09-18 16:16:08 +0300

    Delete event files from storage on destroy

--- astro/src/controllers/events/delete.ts
+++ astro/src/controllers/events/delete.ts
@@ -1,3 +1,4 @@
+import supabaseClient from '@lib/supabaseClient';
 import { PrismaEvent } from '@models';
 import { z } from 'zod';
 
@@ -6,6 +7,15 @@ const input = z.number();
 type Input = z.infer<typeof input>;
 
 const resolve = async ({ input: id }: { input: Input }) => {
+  const event = await PrismaEvent.findUnique({ where: { id } });
+  if (!event) return;
+  const filesToDelete: string[] = [];
+  const { timetableFileKey, resultsFileKey } = event;
+  if (timetableFileKey) filesToDelete.push(timetableFileKey.slice(6));
+  if (resultsFileKey) filesToDelete.push(resultsFileKey.slice(6));
+  if (filesToDelete.length) {
+    await supabaseClient.storage.from('files').remove(filesToDelete);
+  }
   await PrismaEvent.delete({ where: { id } });
 };
 
