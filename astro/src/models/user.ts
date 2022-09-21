bc85ca7444f3406bd32f04eb80dbd9eb7886015b
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-09-21 20:29:52 +0300

    Fetch athletes from db for athletes page

--- astro/src/models/user.ts
+++ astro/src/models/user.ts
@@ -1,7 +1,17 @@
 import prisma from '@lib/prisma';
 import type { User } from '@prisma/client';
 
-const PrismaUser = Object.assign(prisma.user, {});
+const PrismaUser = Object.assign(prisma.user, {
+  async findAllAthletes() {
+    return await prisma.user.findMany({
+      where: { isAthlete: true },
+      include: {
+        athleteProfile: true,
+      },
+      orderBy: { name: 'asc' },
+    });
+  },
+});
 
 export interface UserWithImage extends User {
   image?: string;
